abec1eefdf4f5c3a1dcb5472e12a55692a2b92bc
Jonathan Rial <jonathan.rial@hefr.ch>
2019-03-15 08:50:53 +0100

    Add filters

--- src/main/java/MyCrawler.java
+++ src/main/java/MyCrawler.java
@@ -2,14 +2,23 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+import java.util.regex.Pattern;
+
 public class MyCrawler extends WebCrawler {
 
-    @Override
-    public boolean shouldVisit(Page referringPage, WebURL url) {
-        return false;
-    }
+  private static final Pattern FILTERS =
+      Pattern.compile(
+          ".*(\\.(css|js|bmp|gif|jpe?g"
+              + "|png|tiff?|mid|mp2|mp3|mp4"
+              + "|wav|avi|mov|mpeg|ram|m4v|pdf"
+              + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
-    @Override
-    public void visit(Page page) {
-    }
+  @Override
+  public boolean shouldVisit(Page referringPage, WebURL url) {
+    String href = url.getURL().toLowerCase();
+    return !FILTERS.matcher(href).matches() && href.startsWith("https://www.vegan.com/");
+  }
+
+  @Override
+  public void visit(Page page) {}
 }
