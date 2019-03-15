08bfbf3c5cb43c64d374247129f5ca11a75cc66e
Jonathan Rial <jonathan.rial@hefr.ch>
2019-03-15 09:11:43 +0100

    Add Solr Library

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,43 +6,43 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class Controller {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int numberOfCrawlers = 10;
+        int numberOfCrawlers = 10;
 
-    /*
-     * Crawler4J configuration
-     */
-    CrawlConfig config = new CrawlConfig();
-    config.setMaxConnectionsPerHost(10);
-    config.setConnectionTimeout(4000);
-    config.setSocketTimeout(5000);
-    config.setCrawlStorageFolder("tmp");
-    config.setIncludeHttpsPages(true);
+        /*
+         * Crawler4J configuration
+         */
+        CrawlConfig config = new CrawlConfig();
+        config.setMaxConnectionsPerHost(10);
+        config.setConnectionTimeout(4000);
+        config.setSocketTimeout(5000);
+        config.setCrawlStorageFolder("tmp");
+        config.setIncludeHttpsPages(true);
 
-    // minimum 250ms for tests
-    config.setPolitenessDelay(250);
-    config.setUserAgentString("crawler4j/WEM/2019");
+        // minimum 250ms for tests
+        config.setPolitenessDelay(250);
+        config.setUserAgentString("crawler4j/WEM/2019");
 
-    // max 2-3 levels for tests on large website
-    config.setMaxDepthOfCrawling(8);
+        // max 2-3 levels for tests on large website
+        config.setMaxDepthOfCrawling(8);
 
-    // -1 for unlimited number of pages
-    config.setMaxPagesToFetch(1000);
+        // -1 for unlimited number of pages
+        config.setMaxPagesToFetch(1000);
 
-    /*
-     * Instantiate the controller for this crawl.
-     */
-    PageFetcher pageFetcher = new PageFetcher(config);
-    RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-    RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
 
-    try {
-      CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-      controller.addSeed("https://www.vegan.com/");
-      controller.start(MyCrawler.class, numberOfCrawlers);
-    } catch (Exception e) {
-      e.printStackTrace();
+        try {
+            CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+            controller.addSeed("https://www.vegan.com/");
+            controller.start(MyCrawler.class, numberOfCrawlers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
--- src/main/java/MyCrawler.java
+++ src/main/java/MyCrawler.java
@@ -1,24 +1,78 @@
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Pattern;
 
 public class MyCrawler extends WebCrawler {
 
-  private static final Pattern FILTERS =
-      Pattern.compile(
-          ".*(\\.(css|js|bmp|gif|jpe?g"
-              + "|png|tiff?|mid|mp2|mp3|mp4"
-              + "|wav|avi|mov|mpeg|ram|m4v|pdf"
-              + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+    private static final Pattern FILTERS =
+            Pattern.compile(
+                    ".*(\\.(css|js|bmp|gif|jpe?g"
+                            + "|png|tiff?|mid|mp2|mp3|mp4"
+                            + "|wav|avi|mov|mpeg|ram|m4v|pdf"
+                            + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
-  @Override
-  public boolean shouldVisit(Page referringPage, WebURL url) {
-    String href = url.getURL().toLowerCase();
-    return !FILTERS.matcher(href).matches() && href.startsWith("https://www.vegan.com/");
-  }
+    private List<SolrInputDocument> documentsIndexed = new CopyOnWriteArrayList<>();
 
-  @Override
-  public void visit(Page page) {}
+    private static final String SERVER_URL_RIAL = "http://localhost:32768/solr/mycore";
+
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        String href = url.getURL().toLowerCase();
+        return !FILTERS.matcher(href).matches() && href.startsWith("https://www.vegan.com/");
+    }
+
+    @Override
+    public void visit(Page page) {
+        String url = page.getWebURL().getURL();
+        System.out.println("URL: " + url);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            // Parsing Tags out of Jsoup
+            Document doc = Jsoup.parse(html);
+            SolrInputDocument doSolrInputDocument = new SolrInputDocument();
+            doSolrInputDocument.setField("id", page.hashCode());
+            SolrServer solr = new HttpSolrServer(SERVER_URL_RIAL);
+
+            Elements paragraphList = doc.getElementsByTag("p");
+            for (Element parElement : paragraphList) {
+                String paragraphText = parElement.text();
+                doSolrInputDocument.setField("features", paragraphText);
+            }
+
+            documentsIndexed.add(doSolrInputDocument);
+
+            if (documentsIndexed.size() > 1) {
+                try {
+                    solr.add(doSolrInputDocument);
+
+                    solr.commit(true, true);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Text length: " + text.length());
+            System.out.println("Html length: " + html.length());
+            System.out.println("Number of outgoing links: " + links.size());
+        }
+    }
 }
